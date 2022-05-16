54f0f82986a819d4ba9f5a227c36e5e42633db31
shandawei <shandawei@me.com>
2022-05-17 03:00:35 +0800

    优化脚本

--- script/release.sh
+++ script/release.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
+npm run test
 npm version patch
 npm run publish-npm
-appVersion=`node -p -e "require('./package.json').version"
+appVersion=`node -p -e "require('./package.json').version"`
 git push
 git tag -a "v$appVersion" -m "auto release  $appVersion"
 git push origin --tags
