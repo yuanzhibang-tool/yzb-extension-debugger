b98c6498e6d7c9c22fe0a21c8da4f1d21485031b
shandawei <shandawei@me.com>
2022-08-02 12:50:41 +0800

    添加注释,优化调试

--- debug/debug.ts
+++ debug/debug.ts
@@ -1,7 +1,9 @@
 import { extensionDebugger, DebuggerLogger } from '../src';
 DebuggerLogger.withLog = true;
 const viewPath = './debug.html';
-extensionDebugger.startServer(8080, viewPath);
-extensionDebugger.startWsServer(8889);
 extensionDebugger.setExtensionPath("./debug/extension.ts");
-// extensionDebugger.runExtension();
+// !使用http服务调试
+extensionDebugger.startServer(8080, viewPath);
+extensionDebugger.runExtension();
+// !使用ws服务与渲染端联调,请不要使用runExtension,请使用@yuanzhibang/renderer mock worker后调用run执行
+extensionDebugger.startWsServer(8889);
