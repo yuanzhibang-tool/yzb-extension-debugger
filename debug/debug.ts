98becf35670f981730615e8fdaf6412f7d20e6f3
shandawei <shandawei@me.com>
2022-10-27 07:55:35 +0800

    优化提示

--- debug/debug.ts
+++ debug/debug.ts
@@ -3,6 +3,9 @@ DebuggerLogger.withLog = true;
 const viewPath = './debug.html';
 extensionDebugger.setExtensionPath("./debug/extension.ts");
 // Debug with http server, 使用http服务调试, 使用http服務調試
+// There is no need to pass in viewPath during use, at this time pass in for debugging debug.html page 
+// 使用过程中无需传入viewPath, 此时传入用于调试debug.html页面
+// 使用過程中無需傳入viewPath, 此時傳入用於調試debug.html頁面
 extensionDebugger.startServer(8080, viewPath);
 extensionDebugger.runExtension();
 // Use the ws service to debug with the rendering side, please do not use runExtension, please use @yuanzhibang/renderer mock worker and use code below
